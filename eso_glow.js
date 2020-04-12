6e467d8e53cf9e88b91d43c94f5496bddd0a5266
suicidegangxyz <32012110+suicidegangxyz@users.noreply.github.com>
2020-04-13 00:58:35 +0200

    Create eso_glow.js

---
+++ eso_glow.js
@@ -0,0 +1,30 @@
+/**
+ *
+ * Title: Eso glow chams
+ * Author: leed
+ * Credits: VexatiousCheff for material helper js
+*/
+
+var helper = require("module/material_helper.js")
+
+Material.Create("Eso Glow")
+UI.AddColorPicker("Eso Glow color");
+
+function onMat(){
+    var mat_index = Material.Get("Eso Glow")
+    var color = UI.GetColor("Misc", "JAVASCRIPT", "Script Items", "Eso Glow color");
+    var envmap_int = "[" + (color[0]/255.0).toString() + " " + (color[1]/255.0).toString() + " " + (color[2]/255.0).toString() + "]";
+    helper.setMat(mat_index, {
+        envmap: "models/effects/cube_white",
+        envmaptint: envmap_int,
+        envmapfresnel: "1",
+        envmapfresnelminmaxexp: "[0 1 2]",
+        alpha: "1"
+    })
+    Material.Refresh(mat_index);
+}
+function onUnload(){
+    Material.Destroy("Eso Glow")
+}
+Cheat.RegisterCallback("Unload", "onUnload")
+Cheat.RegisterCallback("Material", "onMat")
